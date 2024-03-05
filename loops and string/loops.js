acad58079427590b9aabf451acce662f51171c15
dipu kumar <dipukumardevcod@email.com>
2024-03-06 00:06:42 +0530

    make a game to user match the give hide number

---
+++ loops and string/loops.js
@@ -0,0 +1,40 @@
+// find the all even  number
+
+// let n = 100;
+// for (let i = 1; i <= n; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i)
+//     }
+// }
+
+
+
+// making a game to give a number user and user prompt enter the number if wrong user can enter num else correct so prompt congulation 🎉:
+
+
+// let fixed_num = 25;
+// let user_input = prompt("Enter the fixed_num:");
+// while (user_input != fixed_num) {
+//     user_input = prompt("Enter number is wrong .try agani")
+// }
+// console.log("congulation you find same num")
+
+
+
+
+// using the for-of loops( for-of loops use to string and arrays to go one by one go and itrate)
+
+// let name = "dipu kumar";
+// for(let i of name){
+//     console.log("name word by word:", i)
+// }
+
+
+
+// using the for-in loops (using the for-in loops user to find the object (key and value) );
+
+let self ={
+    Name:"Dipu kumar",
+    Age:20,
+    pass:true
+};
