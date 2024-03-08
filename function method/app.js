ec7a62c37f8fa60d4c5c24338bfc7473f043247a
dipu kumar <dipukumardevcod@email.com>
2024-03-08 18:10:08 +0530

    learn about the functions

---
+++ function method/app.js
@@ -0,0 +1,14 @@
+
+// function use (a block of code that perform a speical take)
+function name(){
+    console.log("java script is a good programing lang");
+};
+// name(); // it is also know as arugumetns;
+
+// Q. make a function to print the sum of two number;
+
+function sum(a,b){
+   console.log(a+b)
+}
+
+sum(4,5);
